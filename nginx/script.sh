67af09d6e1c5aa8522370a5dc03372c2b0aebb9b
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2024-05-23 17:10:50 +0530

    updated script

--- nginx/script.sh
+++ nginx/script.sh
@@ -6,8 +6,11 @@ echo "script running"
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
 # Print the message
+sudo chown -R ubuntu:ubuntu /var/www/html/
 
-sudo echo "Hello from $IP_ADDRESS"  >> /var/www/html/index.nginx-debian.html
+echo "Hello from $IP_ADDRESS"  > /var/www/html/index.nginx-debian.html
+
+sudo chown -R root:root /var/www/html/
 
 sudo systemctl restart nginx.service
 sudo systemctl status nginx.service
